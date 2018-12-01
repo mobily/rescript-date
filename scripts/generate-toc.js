a7c4acd2664e7792010ed96cfac669e388cd9695
Marcin Dziewulski <hello@mobily.pl>
2018-12-01 10:29:15 +0100

    Add `generate-toc` script

---
+++ scripts/generate-toc.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const mdContents = require('markdown-contents')
+const fs = require('fs')
+const path = require('path')
+const { pipe, map, reduce, replace, curry } = require('ramda')
+
+const resolvePath = (...paths) => path.resolve(__dirname, '..', ...paths)
+const pathToReadme = resolvePath('README.md')
+const readFile = file => fs.readFileSync(file, 'utf-8')
+const writeFile = (file, content) => fs.writeFileSync(file, content, { encoding: 'utf-8' })
+const readDocFile = name => {
+  const file = resolvePath('docs', `${name}.md`)
+  return readFile(file)
+}
+const retrieveMarkdownTree = md => mdContents(md).tree()
+const treeToMarkdown = tree => mdContents.treeToMarkdown(tree)
+const pathToDocs = doc => `docs/${doc}.md`
+const generateAnchors = curry((doc, content) => replace(/\(\#(.+)\)/g, `(${pathToDocs(doc)}#$1)`, content))
+
+const docs = ['second', 'hour', 'day']
+const retrieveMarkdownOf = pipe(
+  readDocFile,
+  retrieveMarkdownTree,
+  treeToMarkdown,
+)
+const generateToC = pipe(
+  map(doc => {
+    const content = retrieveMarkdownOf(doc)
+    return generateAnchors(doc, content)
+  }),
+  reduce((acc, toc) => `${acc}${toc}`, ''),
+)
+const saveReadme = content => {
+  const readmeContent = readFile(pathToReadme)
+  const newReadmeContent = replace('[[toc]]', content, readmeContent)
+  writeFile(pathToReadme, newReadmeContent)
+}
+const generateToCAndSaveReadme = pipe(
+  generateToC,
+  saveReadme,
+)
+
+generateToCAndSaveReadme(docs)
